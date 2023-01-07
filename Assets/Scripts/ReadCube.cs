f2058857ce7263f4c3dfefd906e14c92f6e3ab9d
Verity Davis <veritydavis@gmail.com>
2023-01-06 18:11:25 -0800

    Adding Cube Map

---
+++ Assets/Scripts/ReadCube.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadCube : MonoBehaviour
+{
+    public Transform tUp;
+    public Transform tDown;
+    public Transform tLeft;
+    public Transform tRight;
+    public Transform tFront;
+    public Transform tBack;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
