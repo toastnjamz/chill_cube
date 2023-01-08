64d17ba45098e8301f167429d105b893419dbedd
Verity Davis <veritydavis@gmail.com>
2023-01-07 21:39:10 -0800

    Adding first Raycasts and coloring front face orange

--- Assets/Scripts/ReadCube.cs
+++ Assets/Scripts/ReadCube.cs
@@ -10,16 +10,40 @@ public class ReadCube : MonoBehaviour
     public Transform tRight;
     public Transform tFront;
     public Transform tBack;
+
+    private int layerMask = 1 << 8; // this layer mask is for the faces of the cube only
+    CubeState cubeState;
+    CubeMap CubeMap;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        cubeState = FindObjectOfType<CubeState>();
+        CubeMap = FindObjectOfType<CubeMap>();
+
+        List<GameObject> facesHit = new List<GameObject>();
+        Vector3 ray = tFront.transform.position;
+        RaycastHit hit;
+
+        // Does the ray intersect any objects in the layerMask?
+        if (Physics.Raycast(ray, tFront.right, out hit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(ray, tFront.right * hit.distance, Color.yellow);
+            facesHit.Add(hit.collider.gameObject);
+            print(hit.collider.gameObject.name);
+        }
+        else
+        {
+            Debug.DrawRay(ray, tFront.right * 1000, Color.green);
+        }
+
+        cubeState.front = facesHit;
+        CubeMap.Set();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
